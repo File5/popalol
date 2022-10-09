c34c368f8deb8e9e51ab482489ee6940cf81be3b
Aleksandr Zuev <zuev08@gmail.com>
2022-10-09 21:14:37 +0200

    Django template notice page

--- src/notice/views.py
+++ src/notice/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'notice/index.html')
+    text = "Test text"
+    context = {
+        "rows": range(120),
+        "cols": range(60),
+        "text": text,
+        "shorttext": text[len(text) // 3:],
+    }
+    return render(request, 'notice/index.html', context=context)
